b29d96e153804ad72b98385d0ffaa80fdf8f41b6
johnmeshulam <johnmeshulam@gmail.com>
2023-09-04 23:16:24 +0300

    database lib
    Co-authored-by: WiFi <itamaroryan@users.noreply.github.com>

--- libs/database/README.md
+++ libs/database/README.md
@@ -5,3 +5,12 @@ This library was generated with [Nx](https://nx.dev).
 ## Building
 
 Run `nx build database` to build the library.
+
+## Configuring
+
+Applications using this library to connect to a database must have the following env variables defined:
+
+| Variable     | Required | Default (if optional) |
+| :----------- | :------: | --------------------: |
+| MONGODB_URI  |    No    |   mongodb://127.0.0.1 |
+| MONGODB_PORT |    No    |                 27017 |
--- libs/database/src/lib/database.ts
+++ libs/database/src/lib/database.ts
@@ -1,3 +1,22 @@
-export function database(): string {
-  return 'database';
-}
+import { MongoClient } from 'mongodb';
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1';
+const port = process.env.MONGODB_PORT || 27101;
+
+const initDbClient = async () => {
+  const client = new MongoClient(`${connectionString}:${port}`);
+
+  try {
+    await client.connect();
+  } catch (err) {
+    console.error('❌ Unable to connect to mongodb: ', err);
+  }
+
+  return client;
+};
+
+const client = await initDbClient();
+// Add client specific code here (listeners etc)
+
+const db = client.db('lems');
+export default db;
