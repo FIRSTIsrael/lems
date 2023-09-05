93a2bd70af6054755f4df025d0ed9cb7b0ef7f0a
johnmeshulam <johnmeshulam@gmail.com>
2023-09-06 00:49:32 +0300

    add a websocket server to the backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import * as http from 'http';
 import * as path from 'path';
+import * as WebSocket from 'ws';
 
 const app = express();
+const server = http.createServer(app);
+const wsServer = new WebSocket.Server({ server });
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
@@ -9,8 +13,27 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to backend!' });
 });
 
+wsServer.on('connection', (ws: WebSocket) => {
+  //TODO: move boilerplate code into separate functions
+  ws.on('message', (message: string) => {
+    console.log('received: %s', message);
+    if (message.toString().startsWith('broadcast: ')) {
+      wsServer.clients.forEach((client) => {
+        client.send(`Hello, broadcast message -> ${message}`);
+      });
+    } else {
+      ws.send(`Hello, you sent -> ${message}`);
+    }
+  });
+
+  //TODO: we can handle broken WS connections if needed
+
+  // Immediately sends upon connection
+  ws.send('Hi there, I am a WebSocket server');
+});
+
 const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 
