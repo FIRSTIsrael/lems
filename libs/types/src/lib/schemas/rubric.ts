3605fb490e452421a103e1a4dafac5ee508ddf72
johnmeshulam <johnmeshulam@gmail.com>
2023-09-14 15:53:17 +0300

    rubrics now have optional data attribute wrapping the data

--- libs/types/src/lib/schemas/rubric.ts
+++ libs/types/src/lib/schemas/rubric.ts
@@ -17,14 +17,16 @@ export interface Rubric<T extends JudgingCategory> {
   team: ObjectId;
   category: T;
   status: RubricStatus;
-  awards: T extends 'core-values' ? { [key in OptionalAwards]: boolean } : undefined;
-  values: T extends 'core-values'
-    ? { [key in RubricFields<T>]: RubricValue }
-    : {
-        [key in RubricFields<T>]: {
-          //TODO: smart type that ensures 2 keys: one from [0] and one from [1]
-          [key in RubricInnerFields<T>[0] | RubricInnerFields<T>[1]]: RubricValue;
+  data?: {
+    awards: T extends 'core-values' ? { [key in OptionalAwards]: boolean } : undefined;
+    values: T extends 'core-values'
+      ? { [key in RubricFields<T>]: RubricValue }
+      : {
+          [key in RubricFields<T>]: {
+            //TODO: smart type that ensures 2 keys: one from [0] and one from [1]
+            [key in RubricInnerFields<T>[0] | RubricInnerFields<T>[1]]: RubricValue;
+          };
         };
-      };
-  notes: { greatJob: string; thinkAbout: string };
+    notes: { greatJob: string; thinkAbout: string };
+  };
 }
