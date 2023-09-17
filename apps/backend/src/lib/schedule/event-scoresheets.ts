d8dc75a99356e0223ef23abdaebc67aefa4e5302
LPNG1 <itamar.oryan@gmail.com>
2023-09-17 12:04:22 +0300

    Scoresheet parsing!

---
+++ apps/backend/src/lib/schedule/event-scoresheets.ts
@@ -0,0 +1,35 @@
+import { WithId } from 'mongodb';
+import { Team, Scoresheet } from '@lems/types';
+
+export const getEventScoresheets = (
+  teams: Array<WithId<Team>>,
+  rounds: { practice: number; ranking: number }
+): Scoresheet[] => {
+  const scoresheets = [];
+
+  teams.forEach(team => {
+    for (let i = 1; i <= rounds.practice; i++) {
+      const scoresheet: Scoresheet = {
+        team: team._id,
+        stage: 'practice',
+        round: i,
+        status: 'not-started'
+      };
+
+      scoresheets.push(scoresheet);
+    }
+
+    for (let i = 1; i <= rounds.ranking; i++) {
+      const scoresheet: Scoresheet = {
+        team: team._id,
+        stage: 'ranking',
+        round: i,
+        status: 'not-started'
+      };
+
+      scoresheets.push(scoresheet);
+    }
+  });
+
+  return scoresheets;
+};
--- libs/database/src/index.ts
+++ libs/database/src/index.ts
@@ -8,3 +8,4 @@ export * from './lib/crud/teams';
 export * from './lib/crud/matches';
 export * from './lib/crud/sessions';
 export * from './lib/crud/rubrics';
+export * from './lib/crud/scoresheets';
