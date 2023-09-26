5b1dafc55da5591c5067f3ec4f589ab2896baa97
johnmeshulam <johnmeshulam@gmail.com>
2023-09-26 18:08:01 +0300

    edits to event crud

--- apps/backend/src/routers/api/admin/events/index.ts
+++ apps/backend/src/routers/api/admin/events/index.ts
@@ -7,23 +7,20 @@ const router = express.Router({ mergeParams: true });
 
 router.post('/', (req: Request, res: Response) => {
   const body: Event = { ...req.body };
-
-  //TODO: nice syntax for this in the destructuring
   body.startDate = new Date(body.startDate);
   body.endDate = new Date(body.endDate);
 
   if (body) {
     console.log('⏬ Creating Event...');
-    db.addEvent(body)
-      .then(task => {
-        if (task.acknowledged) {
-          console.log('✅ Event created!');
-          return res.json({ ok: true, id: task.insertedId });
-        } else {
-          console.log('❌ Could not create Event');
-          return res.status(500).json({ ok: false });
-        }
-      });
+    db.addEvent(body).then(task => {
+      if (task.acknowledged) {
+        console.log('✅ Event created!');
+        return res.json({ ok: true, id: task.insertedId });
+      } else {
+        console.log('❌ Could not create Event');
+        return res.status(500).json({ ok: false });
+      }
+    });
   } else {
     return res.status(400).json({ ok: false });
   }
--- libs/database/src/lib/crud/events.ts
+++ libs/database/src/lib/crud/events.ts
@@ -1,5 +1,5 @@
-import { Filter, ObjectId } from "mongodb";
-import { Event } from "@lems/types";
+import { Filter } from 'mongodb';
+import { Event } from '@lems/types';
 import db from '../database';
 
 export const getEvent = (filter: Filter<Event>) => {
@@ -14,10 +14,6 @@ export const updateEvent = (filter: Filter<Event>, newEvent: Partial<Event>) => 
   return db.collection<Event>('events').updateOne(filter, { $set: newEvent }, { upsert: true });
 };
 
-export const getTableEvents = (tableId: ObjectId) => {
-  return db.collection<Event>('events').find({ table: tableId }).toArray();
-};
-
 export const addEvent = (event: Event) => {
   return db
     .collection<Event>('events')
@@ -25,23 +21,9 @@ export const addEvent = (event: Event) => {
     .then(response => response);
 };
 
-export const addEvents = (events: Event[]) => {
-  return db
-    .collection<Event>('events')
-    .insertMany(events)
-    .then(response => response);
-};
-
 export const deleteEvent = (filter: Filter<Event>) => {
   return db
     .collection<Event>('events')
     .deleteOne(filter)
-    .then(response => response);
-};
-
-export const deleteTableEvents = (tableId: ObjectId) => {
-  return db
-    .collection<Event>('events')
-    .deleteMany({ table: tableId })
     .then(response => response);
 };
