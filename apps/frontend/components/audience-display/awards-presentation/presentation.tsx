b7b80a79d06a9ca85ff3f879c75ee7da546f3a72
johnmeshulam <johnmeshulam@gmail.com>
2023-10-24 15:58:05 +0300

    add some boierplate code

--- apps/frontend/components/audience-display/awards-presentation/presentation.tsx
+++ apps/frontend/components/audience-display/awards-presentation/presentation.tsx
@@ -1,5 +1,32 @@
-const AwardsPresentation: React.FC = () => {
+import { createContext } from 'react';
+
+type SlideId = string | number;
+
+type PresentationContextType = {
+  presentationId: string | number;
+  slideCount: number;
+  slideIds: SlideId[];
+  activeView: {
+    slideId: SlideId;
+    slideIndex: number;
+    stepIndex: number;
+  };
+  pendingView: {
+    slideId: SlideId;
+    slideIndex: number;
+    stepIndex: number;
+  };
+  skipTo(options: { slideIndex: number; stepIndex: number }): void;
+  stepForward(): void;
+  stepBackward(): void;
+  advanceSlide(): void;
+  regressSlide(): void;
+};
+
+export const PresentationContext = createContext<PresentationContextType>(null as any);
+
+const Presentation: React.FC = () => {
   return <p>yes</p>;
 };
 
-export default AwardsPresentation;
+export default Presentation;
