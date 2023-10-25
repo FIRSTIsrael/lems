71e286db48df8a4a18ab83107de6b9991a4c5af4
johnmeshulam <johnmeshulam@gmail.com>
2023-10-25 10:13:06 +0300

    actually make tm schedule faster

--- apps/frontend/components/tournament-manager/judging-editor-team-cell.tsx
+++ apps/frontend/components/tournament-manager/judging-editor-team-cell.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import { WithId } from 'mongodb';
 import { Field, FieldProps } from 'formik';
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, TextField, Button, Typography } from '@mui/material';
 import { Team } from '@lems/types';
 
 interface JudgingEditorTeamCellProps {
@@ -10,56 +11,85 @@ interface JudgingEditorTeamCellProps {
 }
 
 const JudgingEditorTeamCell: React.FC<JudgingEditorTeamCellProps> = ({ name, teams, disabled }) => {
+  const [editable, setEditable] = useState<boolean>(false);
   let dropdownOptions: Array<WithId<Team> | null> = [null];
   dropdownOptions = dropdownOptions.concat(teams.sort((a, b) => a.number - b.number));
 
   return (
     <Field
       name={name}
-      component={({ field, form }: FieldProps) => (
-        <Autocomplete
-          options={dropdownOptions.sort((a, b) => {
-            if (!a) return -1;
-            if (!b) return 1;
+      component={({ field, form }: FieldProps) =>
+        editable ? (
+          <Autocomplete
+            options={dropdownOptions.sort((a, b) => {
+              if (!a) return -1;
+              if (!b) return 1;
 
-            return (
-              Object.values(form.values).filter(v => v === a).length -
-              Object.values(form.values).filter(v => v === b).length
-            );
-          })}
-          getOptionLabel={team => (team ? team.number.toString() : '-')}
-          groupBy={team =>
-            !team
-              ? ''
-              : Object.values(form.values).filter(v => v === team).length === 0
-              ? 'חסר'
-              : 'קיים'
-          }
-          inputMode="search"
-          disableClearable={field.value !== null}
-          disabled={disabled}
-          renderInput={params => (
-            <TextField
-              {...params}
-              variant="standard"
-              InputProps={{
-                ...params.InputProps,
-                disableUnderline: true
-              }}
-            />
-          )}
-          sx={{
-            '& .MuiAutocomplete-inputRoot': {
-              color:
-                Object.values(form.values).filter(v => v === field.value).length > 1
-                  ? '#f57c00'
-                  : ''
+              return (
+                Object.values(form.values).filter(v => v === a).length -
+                Object.values(form.values).filter(v => v === b).length
+              );
+            })}
+            getOptionLabel={team => (team ? team.number.toString() : '-')}
+            groupBy={team =>
+              !team
+                ? ''
+                : Object.values(form.values).filter(v => v === team).length === 0
+                ? 'חסר'
+                : 'קיים'
             }
-          }}
-          value={field.value}
-          onChange={(_e, newValue) => form.setFieldValue(field.name, newValue)}
-        />
-      )}
+            inputMode="search"
+            disableClearable={field.value !== null}
+            disabled={disabled}
+            renderInput={params => (
+              <TextField
+                {...params}
+                variant="standard"
+                InputProps={{
+                  ...params.InputProps,
+                  disableUnderline: true
+                }}
+              />
+            )}
+            sx={{
+              '& .MuiAutocomplete-inputRoot': {
+                color:
+                  Object.values(form.values).filter(v => v === field.value).length > 1
+                    ? '#f57c00'
+                    : ''
+              }
+            }}
+            value={field.value}
+            onChange={(_e, newValue) => form.setFieldValue(field.name, newValue)}
+          />
+        ) : (
+          <Button
+            variant="text"
+            onClick={e => setEditable(true)}
+            sx={{
+              '&:hover': {
+                backgroundColor:
+                  field.value &&
+                  Object.values(form.values).filter(v => v === field.value).length > 1
+                    ? '#fff3e7'
+                    : '#f7f5f5'
+              }
+            }}
+          >
+            <Typography
+              sx={{
+                color:
+                  field.value &&
+                  Object.values(form.values).filter(v => v === field.value).length > 1
+                    ? '#f57c00'
+                    : '#000'
+              }}
+            >
+              {field.value ? field.value.number.toString() : '-'}
+            </Typography>
+          </Button>
+        )
+      }
     />
   );
 };
