c6964c5bf703b5771deb1805e2fac4a8d8cdce9e
johnmeshulam <johnmeshulam@gmail.com>
2023-10-25 20:29:31 +0300

    drop support for changing event dates

--- apps/frontend/components/admin/edit-event-form.tsx
+++ apps/frontend/components/admin/edit-event-form.tsx
@@ -101,6 +101,7 @@ const EditEventForm: React.FC<EditEventFormProps> = ({ event, onSubmit }) => {
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <Grid xs={6}>
                 <DatePicker
+                  disabled // Currently we do not support editing event dates (see LEMS-153)
                   label="תאריך התחלה"
                   value={startDate}
                   onChange={newDate => {
@@ -115,6 +116,7 @@ const EditEventForm: React.FC<EditEventFormProps> = ({ event, onSubmit }) => {
               </Grid>
               <Grid xs={6}>
                 <DatePicker
+                  disabled // Currently we do not support editing event dates (see LEMS-153)
                   label="תאריך סיום"
                   value={endDate}
                   onChange={newDate => {
