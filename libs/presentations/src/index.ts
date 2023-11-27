3c6be35dbfcaee63fd1e97f651128c3907fc5872
johnmeshulam <johnmeshulam@gmail.com>
2023-11-27 23:04:02 +0200

    both dockers working
    Co-authored-by: WiFi <itamaroryan@users.noreply.github.com>

--- libs/presentations/src/index.ts
+++ libs/presentations/src/index.ts
@@ -1,4 +1,7 @@
-export { Deck, DeckProps, DeckRef } from './lib/components/deck';
-export { Slide, SlideProps } from './lib/components/slide';
+export { Deck } from './lib/components/deck';
+export type { DeckProps, DeckRef } from './lib/components/deck';
+export { Slide } from './lib/components/slide';
+export type { SlideProps } from './lib/components/slide';
 export { Appear, Stepper } from './lib/components/appear';
-export { DeckView, GOTO_FINAL_STEP } from './lib/hooks/use-deck-state';
+export { GOTO_FINAL_STEP } from './lib/hooks/use-deck-state';
+export type { DeckView } from './lib/hooks/use-deck-state';
