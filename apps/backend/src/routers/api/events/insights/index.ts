4d2bbe05888b0e6958be7f0cc7f5f560a7b0c586
johnmeshulam <johnmeshulam@gmail.com>
2023-12-07 20:04:14 +0200

    create insights router

---
+++ apps/backend/src/routers/api/events/insights/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import { ObjectId } from 'mongodb';
+import * as db from '@lems/database';
+
+const router = express.Router({ mergeParams: true });
+
+router.get('/', (req: Request, res: Response) => {
+  db.getEventTeams(new ObjectId(req.params.eventId)).then(teams => {
+    res.json(teams);
+  });
+});
+
+// router.get('/:teamId', (req: Request, res: Response) => {
+//   db.getTeam({
+//     _id: new ObjectId(req.params.teamId),
+//     eventId: new ObjectId(req.params.eventId)
+//   }).then(team => {
+//     res.json(team);
+//   });
+// });
+
+// router.use('/:teamId/rubrics', rubricsRouter);
+
+// router.use('/:teamId/scoresheets', scoresheetsRouter);
+
+export default router;
