02ff7b4f51e43e6eedcd4cb47c8a99a430c1e392
johnmeshulam <johnmeshulam@gmail.com>
2023-12-09 14:40:12 +0200

    working aggregate

--- apps/backend/src/routers/api/events/insights/index.ts
+++ apps/backend/src/routers/api/events/insights/index.ts
@@ -7,9 +7,8 @@ const router = express.Router({ mergeParams: true });
 router.get('/validate-csv-readiness', async (req: Request, res: Response) => {
   const category = req.query.category;
 
-  //TODO: fix validation
-  // if (!((category as string) in JudgingCategoryTypes))
-  //   return res.status(400).json({ error: 'Invalid Category' });
+  if (!JudgingCategoryTypes.some(c => category === c))
+    return res.status(400).json({ error: 'Invalid Category' });
 
   const pipeline = [
     {
@@ -17,20 +16,36 @@ router.get('/validate-csv-readiness', async (req: Request, res: Response) => {
     },
     {
       $project: {
-        'data.values': { $objectToArray: '$scores' }
+        teamId: true,
+        scores: { $objectToArray: '$data.values' }
+      }
+    },
+    {
+      $project: {
+        teamId: true,
+        unscored: {
+          $sum: {
+            $map: {
+              input: '$scores',
+              as: 'element',
+              in: {
+                $cond: {
+                  if: { $eq: ['$$element.v.value', 0] },
+                  then: 1,
+                  else: 0
+                }
+              }
+            }
+          }
+        }
       }
     },
     {
       $match: {
-        $expr: {
-          $eq: [{ $size: { $setDifference: ['$scores.v', [0]] } }, 1]
-        }
+        unscored: { $gt: 0 }
       }
     }
   ];
-
-  //TODO: fix, getting error
-  //MongoServerError: PlanExecutor error during aggregation :: caused by :: The argument to $size must be an array, but was of type: null
 
   const report = await db.db.collection('rubrics').aggregate(pipeline).toArray();
   res.json(report);
