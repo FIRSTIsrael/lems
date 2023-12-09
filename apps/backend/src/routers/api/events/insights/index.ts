78bf04aa5d47979c1fba10292010b63e5abfcf9a
johnmeshulam <johnmeshulam@gmail.com>
2023-12-09 11:43:09 +0200

    progress on rubric readiness query

--- apps/backend/src/routers/api/events/insights/index.ts
+++ apps/backend/src/routers/api/events/insights/index.ts
@@ -1,13 +1,39 @@
 import express, { Request, Response } from 'express';
-import { ObjectId } from 'mongodb';
 import * as db from '@lems/database';
+import { JudgingCategoryTypes } from '@lems/types';
 
 const router = express.Router({ mergeParams: true });
 
-router.get('/', (req: Request, res: Response) => {
-  db.getEventTeams(new ObjectId(req.params.eventId)).then(teams => {
-    res.json(teams);
-  });
+router.get('/validate-csv-readiness', async (req: Request, res: Response) => {
+  const category = req.query.category;
+
+  //TODO: fix validation
+  // if (!((category as string) in JudgingCategoryTypes))
+  //   return res.status(400).json({ error: 'Invalid Category' });
+
+  const pipeline = [
+    {
+      $match: { category, status: { $ne: 'empty' } }
+    },
+    {
+      $project: {
+        'data.values': { $objectToArray: '$scores' }
+      }
+    },
+    {
+      $match: {
+        $expr: {
+          $eq: [{ $size: { $setDifference: ['$scores.v', [0]] } }, 1]
+        }
+      }
+    }
+  ];
+
+  //TODO: fix, getting error
+  //MongoServerError: PlanExecutor error during aggregation :: caused by :: The argument to $size must be an array, but was of type: null
+
+  const report = await db.db.collection('rubrics').aggregate(pipeline).toArray();
+  res.json(report);
 });
 
 export default router;
