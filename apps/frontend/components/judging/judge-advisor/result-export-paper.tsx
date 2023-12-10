97a1a752584861047357213f5cbef7811f6d53d0
johnmeshulam <johnmeshulam@gmail.com>
2023-12-10 22:52:43 +0200

    normal styling

--- apps/frontend/components/judging/judge-advisor/result-export-paper.tsx
+++ apps/frontend/components/judging/judge-advisor/result-export-paper.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { WithId } from 'mongodb';
-import { Button, Paper, Box, Avatar, Typography } from '@mui/material';
+import { Paper, Box, Avatar, Typography } from '@mui/material';
+import RuleIcon from '@mui/icons-material/Rule';
 import ManageIcon from '@mui/icons-material/WidgetsRounded';
 import Grid from '@mui/material/Unstable_Grid2';
-import { Event, JudgingCategory, Team } from '@lems/types';
+import { Event, JudgingCategory, JudgingCategoryTypes, Team } from '@lems/types';
 import { localizedJudgingCategory } from '@lems/season';
 import { apiFetch } from '../../../lib/utils/fetch';
 import ExportAction from './export-action';
@@ -22,7 +23,7 @@ const ResultExportPaper: React.FC<ResultExportPaperProps> = ({ event }) => {
     'robot-design': []
   });
 
-  const fetchUnscoredTeams = () => {
+  useEffect(() => {
     apiFetch(`/api/events/${event._id}/insights/validate-csv-readiness`).then(res =>
       res.json().then(data => {
         const newUnscoredTeams: UnscoredTeamsType = {} as UnscoredTeamsType;
@@ -33,7 +34,8 @@ const ResultExportPaper: React.FC<ResultExportPaperProps> = ({ event }) => {
         setUnscoredTeams(newUnscoredTeams);
       })
     );
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Paper sx={{ borderRadius: 3, mb: 4, boxShadow: 2, p: 3 }}>
@@ -61,42 +63,50 @@ const ResultExportPaper: React.FC<ResultExportPaperProps> = ({ event }) => {
         </Typography>
       </Box>
       <Grid container spacing={2}>
-        <Grid xs={6}>
-          <ExportAction event={event} path="/rubrics/core-values" sx={{ m: 1 }}>
-            ייצוא מחווני ערכי הליבה
-          </ExportAction>
-        </Grid>
-        <Grid xs={6}>
-          <ExportAction event={event} path="/rubrics/innovation-project" sx={{ m: 1 }}>
-            ייצוא מחווני פרויקט החדשנות
-          </ExportAction>
-        </Grid>
-        <Grid xs={6}>
-          <ExportAction event={event} path="/rubrics/robot-design" sx={{ m: 1 }}>
-            ייצוא מחווני תכנון הרובוט
-          </ExportAction>
-        </Grid>
+        {JudgingCategoryTypes.map(category => {
+          const teams = unscoredTeams[category];
+          return (
+            <React.Fragment key={category}>
+              <Grid xs={6}>
+                <ExportAction event={event} path={`/rubrics/${category}`} sx={{ m: 1 }}>
+                  {`ייצוא מחווני ${localizedJudgingCategory[category].name}`}
+                </ExportAction>
+              </Grid>
+              <Grid xs={6}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center'
+                  }}
+                  key={category}
+                >
+                  <Avatar
+                    sx={{
+                      bgcolor: '#fadaca',
+                      color: '#d4642c',
+                      width: '1.5rem',
+                      height: '1.5rem',
+                      mr: 1
+                    }}
+                  >
+                    <RuleIcon sx={{ fontSize: '0.75rem' }} />
+                  </Avatar>
+                  <Typography variant="h2" fontSize="1.25rem">
+                    <Typography>
+                      {teams.length > 5 ? 'יותר מ-5 קבוצות' : teams.map(t => t.number).join(', ')}
+                    </Typography>
+                  </Typography>
+                </Box>
+              </Grid>
+            </React.Fragment>
+          );
+        })}
+
         <Grid xs={6}>
           <ExportAction event={event} path="/scores" sx={{ m: 1 }}>
             ייצוא תוצאות זירה
           </ExportAction>
-        </Grid>
-        <Grid xs={6}>
-          <Button variant="contained" onClick={() => fetchUnscoredTeams()}>
-            בדיקת מוכנות לייצוא
-          </Button>
-        </Grid>
-        <Grid xs={6}>
-          {Object.entries(unscoredTeams).map(entry => {
-            const category = entry[0] as JudgingCategory;
-            const teams = entry[1];
-            if (teams.length === 0) return;
-            return (
-              <Typography key={category}>{`${
-                localizedJudgingCategory[category].name
-              }: ${JSON.stringify(teams.map(t => t.number))}`}</Typography>
-            );
-          })}
         </Grid>
       </Grid>
     </Paper>
