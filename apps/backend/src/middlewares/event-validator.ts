994ab3493d6bccc56ab75522e6f2ccda9ab53adf
johnmeshulam <johnmeshulam@gmail.com>
2023-12-14 17:34:54 +0200

    add validator middleware

--- apps/backend/src/middlewares/event-validator.ts
+++ apps/backend/src/middlewares/event-validator.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import { ObjectId } from 'mongodb';
 
 const eventValidator = async (req: Request, res: Response, next: NextFunction) => {
+  if (!ObjectId.isValid(req.params.eventId))
+    return res.status(400).json({ error: 'Invalid event' });
+
   if (req.user?.eventId?.toString() === req.params.eventId || req.user.isAdmin) {
     return next();
   }
