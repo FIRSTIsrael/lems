e4425f3024ff823a2f4db830398824dc7903013a
johnmeshulam <johnmeshulam@gmail.com>
2024-01-13 11:54:12 +0200

    insights page

---
+++ apps/frontend/pages/event/[eventId]/insights.tsx
@@ -0,0 +1,82 @@
+import { GetServerSideProps, NextPage } from 'next';
+import router from 'next/router';
+import { WithId } from 'mongodb';
+import { useState } from 'react';
+import { enqueueSnackbar } from 'notistack';
+import { Tabs, Tab, Paper, Stack, Typography } from '@mui/material';
+import { TabContext, TabPanel } from '@mui/lab';
+import { Event, EventState, SafeUser } from '@lems/types';
+import ConnectionIndicator from '../../../components/connection-indicator';
+import Layout from '../../../components/layout';
+import ReportLink from '../../../components/general/report-link';
+import { RoleAuthorizer } from '../../../components/role-authorizer';
+import TicketCreationPanel from '../../../components/pit-admin/ticket-creation-panel';
+import TeamRegistrationPanel from '../../../components/pit-admin/team-registration-panel';
+import { apiFetch, serverSideGetRequests } from '../../../lib/utils/fetch';
+import { localizedRoles } from '../../../localization/roles';
+import TicketPanel from '../../../components/general/ticket-panel';
+
+interface Props {
+  user: WithId<SafeUser>;
+  event: WithId<Event>;
+  eventState: WithId<EventState>;
+}
+
+const Page: NextPage<Props> = ({ user, event, eventState }) => {
+  const [activeTab, setActiveTab] = useState<string>('1');
+
+  return (
+    <RoleAuthorizer
+      user={user}
+      allowedRoles={['judge-advisor', 'lead-judge', 'head-referee', 'tournament-manager']}
+      onFail={() => {
+        router.push(`/event/${event._id}/${user.role}`);
+        enqueueSnackbar('לא נמצאו הרשאות מתאימות.', { variant: 'error' });
+      }}
+    >
+      <Layout maxWidth="md" title={`ממשק תובנות | ${event.name}`}>
+        {eventState.completed ? (
+          <TabContext value={activeTab}>
+            <Paper sx={{ mt: 4 }}>
+              <Tabs
+                value={activeTab}
+                onChange={(_e, newValue: string) => setActiveTab(newValue)}
+                centered
+              >
+                <Tab label="שיפוט" value="1" />
+                <Tab label="זירה" value="2" />
+                <Tab label="כללי" value="3" />
+              </Tabs>
+            </Paper>
+            <TabPanel value="1"></TabPanel>
+            <TabPanel value="2"></TabPanel>
+            <TabPanel value="3">\ </TabPanel>
+          </TabContext>
+        ) : (
+          //TODO: normal incomplete page, maybe redirect
+          <Typography>Event not yes compelted</Typography>
+        )}
+      </Layout>
+    </RoleAuthorizer>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  try {
+    const user = await apiFetch(`/api/me`, undefined, ctx).then(res => res?.json());
+
+    const data = await serverSideGetRequests(
+      {
+        event: `/api/events/${user.eventId}`,
+        eventState: `/api/events/${user.eventId}/state`
+      },
+      ctx
+    );
+
+    return { props: { user, ...data } };
+  } catch (err) {
+    return { redirect: { destination: '/login', permanent: false } };
+  }
+};
+
+export default Page;
