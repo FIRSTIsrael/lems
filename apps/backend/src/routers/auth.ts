1170aecfa0876fc62d4f91d72390fc9baef4cad8
johnmeshulam <johnmeshulam@gmail.com>
2024-01-25 12:32:52 +0200

    revert captcha log

--- apps/backend/src/routers/auth.ts
+++ apps/backend/src/routers/auth.ts
@@ -17,11 +17,7 @@ router.post('/login', async (req: Request, res: Response, next: NextFunction) =>
   if (process.env.RECAPTCHA === 'true') {
     const captcha: RecaptchaResponse = await getRecaptchaResponse(captchaToken);
     if (!captcha.success || captcha['error-codes']?.length > 0) {
-      console.log(
-        `🔏 [${captcha.challenge_ts}] Captcha failure from ${captcha.hostname}: ${
-          captcha['error-codes'] || []
-        }`
-      );
+      console.log(`🔏 Captcha failure: ${captcha['error-codes'] || []}`);
     }
 
     if (captcha.action != 'submit' || captcha.score < 0.5)
