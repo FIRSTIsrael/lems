cf3cc844df744f82f054ac5296976de5639f5aa8
johnmeshulam <johnmeshulam@gmail.com>
2024-02-03 12:17:58 +0200

    Remove CSV validation

--- apps/backend/src/routers/api/events/insights/judging/index.ts
+++ apps/backend/src/routers/api/events/insights/judging/index.ts
@@ -242,66 +242,6 @@ router.get('/robot-room-correlation-to-robot-game', async (req: Request, res: Re
   res.json(report);
 });
 
-router.get('/validate-csv-readiness', async (req: Request, res: Response) => {
-  const pipeline = [
-    {
-      $match: { eventId: new ObjectId(req.params.eventId), status: { $ne: 'empty' } }
-    },
-    {
-      $project: {
-        category: true,
-        teamId: true,
-        scores: { $objectToArray: '$data.values' }
-      }
-    },
-    {
-      $addFields: {
-        unscored: {
-          $sum: {
-            $map: {
-              input: '$scores',
-              as: 'element',
-              in: {
-                $cond: {
-                  if: { $eq: ['$$element.v.value', 0] },
-                  then: 1,
-                  else: 0
-                }
-              }
-            }
-          }
-        }
-      }
-    },
-    {
-      $match: {
-        unscored: { $gt: 0 }
-      }
-    },
-    {
-      $lookup: {
-        from: 'teams',
-        localField: 'teamId',
-        foreignField: '_id',
-        as: 'team'
-      }
-    },
-    {
-      $unwind: '$team'
-    },
-    {
-      $group: {
-        _id: '$category',
-        unscoredTeams: { $addToSet: '$team' }
-      }
-    }
-  ];
-
-  const report = await db.db.collection('rubrics').aggregate(pipeline).toArray();
-  report.sort((a, b) => a.id.localeCompare(b.id));
-  res.json(report);
-});
-
 router.use('/scores', scoresRouter);
 
 export default router;
