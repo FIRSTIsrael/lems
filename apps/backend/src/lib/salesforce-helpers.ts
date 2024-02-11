04f811fb28f35f489d07877239bd6e916b8c4f59
johnmeshulam <johnmeshulam@gmail.com>
2024-02-11 19:58:23 +0200

    fix middleware

--- apps/backend/src/lib/salesforce-helpers.ts
+++ apps/backend/src/lib/salesforce-helpers.ts
@@ -10,5 +10,5 @@ export const getEventBySalesforceIdAndTeamNumber = async (
   const eventTeams = await Promise.all(events.map(e => db.getEventTeams(e._id)));
   const allEventTeams: Array<WithId<Team>> = eventTeams.reduce((acc, arr) => acc.concat(arr));
   const team = allEventTeams.find(t => t.number === teamNumber);
-  return events.find(e => e._id === team.eventId);
+  return events.find(e => e._id.toString() === team.eventId.toString());
 };
