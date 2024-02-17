6d81f2d2b175c5842ea4ff29965582b018fe2d7c
Oshaim <oshaim10@gmail.com>
2024-02-17 22:13:35 +0200

    add queue status to active match

--- apps/frontend/components/field/scorekeeper/active-match.tsx
+++ apps/frontend/components/field/scorekeeper/active-match.tsx
@@ -7,6 +7,7 @@ import WarningAmberRoundedIcon from '@mui/icons-material/WarningAmberRounded';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
+import StartRoundedIcon from '@mui/icons-material/StartRounded';
 import { JudgingSession, MATCH_LENGTH, RobotGameMatch } from '@lems/types';
 import Countdown from '../../general/countdown';
 
@@ -101,6 +102,10 @@ const ActiveMatch: React.FC<ActiveMatchProps> = ({
                         {participant.present === 'present' ? (
                           <Tooltip title="הקבוצה על המגרש" arrow>
                             <DoneRoundedIcon />
+                          </Tooltip>
+                        ) : participant.queued ? (
+                          <Tooltip title="הקבוצה בקיו" arrow>
+                            <StartRoundedIcon />
                           </Tooltip>
                         ) : !participant.team?.registered ? (
                           <Tooltip title="הקבוצה טרם הגיעה לאירוע!" arrow>
