2ed8d6ec7b3eaf6c282e1dae3fca8c9a13171437
johnmeshulam <johnmeshulam@gmail.com>
2024-02-17 12:03:56 +0200

    add routes for backend

---
+++ apps/backend/src/routers/dashboard/events/teams/export.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import * as db from '@lems/database';
+
+const router = express.Router({ mergeParams: true });
+
+router.get(
+  '/rubrics',
+  asyncHandler(async (req: Request, res: Response) => {
+    res.status(501).json({ error: 'NOT IMPLEMENTED' });
+  })
+);
+
+router.get(
+  '/scoresheets',
+  asyncHandler(async (req: Request, res: Response) => {
+    res.status(501).json({ error: 'NOT IMPLEMENTED' });
+  })
+);
+
+router.get(
+  '/awards',
+  asyncHandler(async (req: Request, res: Response) => {
+    res.status(501).json({ error: 'NOT IMPLEMENTED' });
+  })
+);
+
+export default router;
