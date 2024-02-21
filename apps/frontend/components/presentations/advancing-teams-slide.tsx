af8924af0fc855ca6b5455b7783db38d3f58d48c
johnmeshulam <johnmeshulam@gmail.com>
2024-02-21 18:52:54 +0200

    Fix stying on advancing teams slide

--- apps/frontend/components/presentations/advancing-teams-slide.tsx
+++ apps/frontend/components/presentations/advancing-teams-slide.tsx
@@ -1,5 +1,7 @@
 import { WithId } from 'mongodb';
+import { useMemo } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import { Appear, Slide } from '@lems/presentations';
 import { DivisionColor, Team } from '@lems/types';
 import { getDivisionColor } from '../../lib/utils/colors';
@@ -12,29 +14,44 @@ interface AdvancingTeamsSlideProps {
 }
 
 const AdvancingTeamsSlide: React.FC<AdvancingTeamsSlideProps> = ({ teams, color }) => {
+  const COLUMN_SPLIT_THRESHOLD = 11;
+  const splitTeamsIntoColumns = useMemo(() => teams.length > COLUMN_SPLIT_THRESHOLD, [teams]);
+
   return (
     <Slide>
-      <Stack px={20} height="calc(100% - 100px)">
+      <Stack
+        height="calc(100% - 100)"
+        width={splitTeamsIntoColumns ? '100%' : '70%'}
+        alignItems={'center'}
+        sx={{ position: 'absolute', top: 15 }}
+      >
         <Typography variant="h1" fontSize="6rem" textAlign="center" gutterBottom>
           קבוצות המעפילות שלב
         </Typography>
         <Box
           sx={{
             background: '#f7f8f9',
-            maxWidth: 'lg',
-            px: 8,
-            py: 6,
+            mx: 4,
+            px: 4,
+            py: 4,
             borderRadius: 4,
             boxShadow: color && `-20px 20px 24px ${getDivisionColor(color)}74`
           }}
         >
-          {teams.map(team => (
-            <Appear key={team._id.toString()}>
-              <Typography fontSize="1.75rem" fontWeight={500}>
-                {localizeTeam(team)}
-              </Typography>
-            </Appear>
-          ))}
+          <Grid container columns={splitTeamsIntoColumns ? 2 : 1} spacing={1}>
+            {teams.map(team => (
+              <Grid key={team._id.toString()} xs={1}>
+                <Appear>
+                  <Typography
+                    fontSize={splitTeamsIntoColumns ? '2rem' : '2.35rem'}
+                    fontWeight={500}
+                  >
+                    {localizeTeam(team)}
+                  </Typography>
+                </Appear>
+              </Grid>
+            ))}
+          </Grid>
         </Box>
       </Stack>
       <LogoStack />
