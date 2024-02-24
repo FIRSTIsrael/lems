1eea68f3cee612965f9024c5b60f6e5906e74a9c
johnmeshulam <johnmeshulam@gmail.com>
2024-02-24 20:20:01 +0200

    add fileupload

--- apps/backend/src/routers/dashboard/events/teams/upload.ts
+++ apps/backend/src/routers/dashboard/events/teams/upload.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
+import fileUpload from 'express-fileupload';
 import * as db from '@lems/database';
 
 const router = express.Router({ mergeParams: true });
 
 router.post(
   '/team-info',
+  fileUpload(),
   asyncHandler(async (req: Request, res: Response) => {
     res.status(501).json({ error: 'NOT IMPLEMENTED' });
   })
@@ -13,6 +15,7 @@ router.post(
 
 router.get(
   '/robot-design-notebook',
+  fileUpload(),
   asyncHandler(async (req: Request, res: Response) => {
     res.status(501).json({ error: 'NOT IMPLEMENTED' });
   })
