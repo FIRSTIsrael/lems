b2f54a2d388d19e8623dae8de8674c2d90cb2e2e
johnmeshulam <johnmeshulam@gmail.com>
2024-02-25 17:57:04 +0200

    refator uploads

---
+++ apps/backend/src/lib/upload.ts
@@ -0,0 +1,15 @@
+import { ObjectCannedACL, PutObjectCommand } from '@aws-sdk/client-s3';
+import { s3Client } from './s3-client';
+
+export const uploadFile = async (data: Buffer, key: string) => {
+  const bucket = process.env.DIGITALOCEAN_SPACE;
+  const bucketParams = {
+    ACL: 'public-read' as ObjectCannedACL,
+    Bucket: bucket,
+    Key: key,
+    Body: data
+  };
+
+  await s3Client.send(new PutObjectCommand(bucketParams));
+  return `${bucket}/${key}`;
+};
--- apps/backend/src/routers/dashboard/events/teams/upload.ts
+++ apps/backend/src/routers/dashboard/events/teams/upload.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 import fileUpload from 'express-fileupload';
-import * as db from '@lems/database';
+import { ObjectCannedACL, PutObjectCommand } from '@aws-sdk/client-s3';
+import { s3Client } from '../../../../lib/s3-client';
+import { uploadFile } from '../../../../lib/upload';
 
 const router = express.Router({ mergeParams: true });
 
@@ -9,7 +11,13 @@ router.post(
   '/team-info',
   fileUpload(),
   asyncHandler(async (req: Request, res: Response) => {
-    res.status(501).json({ error: 'NOT IMPLEMENTED' });
+    const pdfData = (req.files.file as fileUpload.UploadedFile)?.data;
+    const path = await uploadFile(
+      pdfData,
+      `${req.params.eventId}/teams/${req.teamNumber}/team-info.pdf`
+    );
+    console.log('Successfully uploaded object: ' + path);
+    res.json({ ok: true });
   })
 );
 
@@ -17,7 +25,13 @@ router.get(
   '/robot-design-notebook',
   fileUpload(),
   asyncHandler(async (req: Request, res: Response) => {
-    res.status(501).json({ error: 'NOT IMPLEMENTED' });
+    const pdfData = (req.files.file as fileUpload.UploadedFile)?.data;
+    const path = await uploadFile(
+      pdfData,
+      `${req.params.eventId}/teams/${req.teamNumber}/robot-design-notebook.pdf`
+    );
+    console.log('Successfully uploaded object: ' + path);
+    res.json({ ok: true });
   })
 );
 export default router;
