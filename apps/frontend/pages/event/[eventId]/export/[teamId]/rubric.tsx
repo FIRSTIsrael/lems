9899dce6950f7aa63f262ea3b30fe01ef7d64fb4
johnmeshulam <johnmeshulam@gmail.com>
2024-02-27 14:15:12 +0200

    framework for accessing frontend

---
+++ apps/frontend/pages/event/[eventId]/export/[teamId]/rubric.tsx
@@ -0,0 +1,2 @@
+// Should be a page for a rubric OR a page for all rubrics
+// (But the PDF needs to be split into 1 rubric per page)
---
+++ apps/frontend/pages/event/[eventId]/export/[teamId]/scoresheet.tsx
@@ -0,0 +1,2 @@
+// Should be a page for a scoresheet OR a page for all scoresheets
+// (But the PDF needs to be split into 0.5 scoresheets per page or however fits)
