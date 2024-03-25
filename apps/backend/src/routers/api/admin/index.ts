9571f7fbceced83ccd8d701eb3743a601896d111
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2024-03-25 16:37:06 +0200

    removed redundant use of `csrfValidator`

--- apps/backend/src/routers/api/admin/index.ts
+++ apps/backend/src/routers/api/admin/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import adminEventRouter from './events/index';
 import adminValidator from '../../../middlewares/admin-validator';
-import csrfValidator from '../../../middlewares/csrf-validator';
 
 const router = express.Router({ mergeParams: true });
 
 router.use('/', adminValidator);
-router.post('/', csrfValidator);
 
 router.use('/events', adminEventRouter);
 
