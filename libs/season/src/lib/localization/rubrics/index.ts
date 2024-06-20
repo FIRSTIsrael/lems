ce463ef305a8aade58f6c140ec5378509ba30501
johnmeshulam <johnmeshulam@gmail.com>
2024-06-21 00:23:42 +0300

    make it compile

--- libs/season/src/lib/localization/rubrics/index.ts
+++ libs/season/src/lib/localization/rubrics/index.ts
@@ -4,7 +4,7 @@ import robotDesignSchema from './robot-design';
 import { RubricsSchema } from './typing';
 import { JudgingCategory } from '@lems/types';
 
-export const rubricsSchemas: { [K in JudgingCategory]: RubricsSchema<K> } = {
+export const rubricsSchemas: { [T in JudgingCategory]: RubricsSchema } = {
   'core-values': coreValuesSchema,
   'innovation-project': innovationProjectSchema,
   'robot-design': robotDesignSchema
