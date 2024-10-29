935f92255fc39e4c1f76799809e5d86179dd2157
johnmeshulam <johnmeshulam@gmail.com>
2024-10-29 19:15:14 +0200

    work on feedbacl row styling

--- apps/frontend/components/judging/rubrics/feedback-row.tsx
+++ apps/frontend/components/judging/rubrics/feedback-row.tsx
@@ -1,4 +1,4 @@
-import { Divider, Paper, Stack, Typography } from '@mui/material';
+import { Divider, Paper, Stack, Typography, Box } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import FormikTextField from '../../general/forms/formik-text-field';
 import Markdown from 'react-markdown';
@@ -27,10 +27,11 @@ const FeedbackRow: React.FC<FeedbackRowProps> = ({
       <Stack direction="row" divider={<Divider orientation="vertical" flexItem />}>
         {Object.values(feedback).map((value, index) => (
           <Typography
+            key={index}
             fontWeight={600}
             sx={{
               width: '100%',
-              height: 40,
+              height: 35,
               display: 'flex',
               justifyContent: 'center',
               alignItems: 'center'
@@ -41,18 +42,22 @@ const FeedbackRow: React.FC<FeedbackRowProps> = ({
         ))}
       </Stack>
       <Grid size={12} px={2} sx={{ background: CategoryColors[category] }}>
-        <Markdown>{description}</Markdown>
+        <Box height={25} justifyContent="center" marginTop={-1} marginBottom={0.5}>
+          <Markdown>{description}</Markdown>
+        </Box>
       </Grid>
       <Stack direction="row" divider={<Divider orientation="vertical" flexItem />}>
-        {Object.entries(feedback).map(([key, value]) => (
+        {Object.entries(feedback).map(([key, value], index) => (
           <FormikTextField
             key={key}
             name={`feedback.${key}`}
-            label={value}
             disabled={!isEditable}
             spellCheck
             multiline
             minRows={4}
+            slotProps={{
+              input: { sx: { borderRadius: `0 0 ${index === 0 ? '0 12px' : '12px 0'}` } }
+            }}
           />
         ))}
       </Stack>
