85ce95c63b6b9f53dacb915efa3292d81316071f
johnmeshulam <johnmeshulam@gmail.com>
2024-12-01 22:57:24 +0200

    update dashboard router

--- apps/backend/src/middlewares/dashboard/auth.ts
+++ apps/backend/src/middlewares/dashboard/auth.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 import { DashboardTokenData } from '../../types/auth';
 import { extractToken } from '../../lib/auth';
-import { getDivisionBySalesforceIdAndTeamNumber } from '../../lib/salesforce-helpers';
+import { getDivisionByEventAndTeamNumber } from '../../lib/salesforce-helpers';
 import * as db from '@lems/database';
 
 const dashboardJwtSecret = process.env.DASHBOARD_JWT_SECRET;
@@ -12,8 +12,8 @@ export const dashboardAuthMiddleware = async (req: Request, res: Response, next:
     const token = extractToken(req);
     const tokenData = jwt.verify(token, dashboardJwtSecret) as DashboardTokenData;
 
-    const event = await db.getFllEvent({ salesforceId: tokenData.eventSalesforceId });
-    const division = await db.getEventDivisions(event._id)[0];
+    const event = await db.getFllEvent({ salesforceId: tokenData.eventSalesforceId }); // Assuming unique per event
+    const division = await getDivisionByEventAndTeamNumber(event, tokenData.teamNumber);
     if (!division) throw new Error();
 
     req.event = event;
