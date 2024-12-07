f2088cd458d6a33395d2c87d14ca69031f3c5b63
johnmeshulam <johnmeshulam@gmail.com>
2024-12-07 17:02:38 +0200

    fix use query param

--- apps/frontend/hooks/use-query-param.ts
+++ apps/frontend/hooks/use-query-param.ts
@@ -1,14 +1,26 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export const useQueryParam = (
   param: string,
   initialState: string,
-  shallow = true
-): [string, (newValue: string) => Promise<boolean>] => {
+  refresh = false,
+  shallow = false
+): [string, (newValue: string) => void] => {
   const router = useRouter();
   const { [param]: value } = router.query;
-  const setValue = (newValue: string) =>
-    router.push({ query: { ...router.query, [param]: newValue } }, undefined, { shallow });
-  if (!value) setValue(initialState);
-  return [(value ?? initialState) as string, setValue];
+  const [_value, _setValue] = useState(value ?? initialState);
+
+  const setValue = (newValue: string) => {
+    if (refresh || !('URLSearchParams' in window)) {
+      router.push({ query: { ...router.query, [param]: newValue } }, undefined, { shallow });
+    } else {
+      const url = new URL(window.location.toString());
+      url.searchParams.set(param, newValue);
+      history.pushState(null, '', url);
+    }
+    _setValue(newValue);
+  };
+
+  return [_value as string, setValue];
 };
