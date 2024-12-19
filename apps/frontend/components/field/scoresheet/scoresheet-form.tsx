e0b1d0338c8cab9a1c39ca17b98c2b622ccddcdb
YHerm <0549405422.yh@gmail.com>
2024-12-19 11:46:43 +0200

    read only for referee, fix continue button

--- apps/frontend/components/field/scoresheet/scoresheet-form.tsx
+++ apps/frontend/components/field/scoresheet/scoresheet-form.tsx
@@ -68,7 +68,7 @@ const ScoresheetForm: React.FC<ScoresheetFormProps> = ({
 }) => {
   const router = useRouter();
   const [readOnly, setReadOnly] = useState<boolean>(
-    user.role === 'head-referee' && !['empty', 'waiting-for-head-ref'].includes(scoresheet.status)
+    user.role === 'head-referee' && !['empty', 'waiting-for-head-ref', 'waiting-for-head-ref-gp'].includes(scoresheet.status)
   );
 
   interface ErrorWithMessage {
@@ -472,7 +472,7 @@ const ScoresheetForm: React.FC<ScoresheetFormProps> = ({
                       sx={{ minWidth: 200 }}
                       endIcon={<ChevronLeftIcon />}
                       disabled={!isValid}
-                      onClick={() => handleSync(true, values, 'waiting-for-gp', true)}
+                      onClick={() => handleSync(true, values, user.role === 'head-referee' ? 'waiting-for-head-ref-gp' : 'waiting-for-gp', true)}
                     >
                       המשך
                     </Button>
