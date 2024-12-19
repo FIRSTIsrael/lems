8fb89a7a8d7f3d4d3b8c0d9eac0088883650e1fb
YHerm <0549405422.yh@gmail.com>
2024-12-19 12:25:36 +0200

    fix in edit scoresheet button, need to change color

--- apps/frontend/components/field/head-referee/edit-scoresheet-button.tsx
+++ apps/frontend/components/field/head-referee/edit-scoresheet-button.tsx
@@ -58,6 +58,16 @@ const mapStatus: {
       }
     }
   },
+  'waiting-for-head-ref-gp': {
+    title: 'ממתין לציון מקצועיות אדיבה משופט ראשי',
+    sx: {
+      color: '#fff',
+      background: '#ff9800',
+      '&:hover': {
+        background: '#f79300'
+      }
+    }
+  },
   ready: {
     title: 'הוגש',
     sx: {
