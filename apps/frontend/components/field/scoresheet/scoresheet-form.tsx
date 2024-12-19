0ed45dbdcc3e08e756127af1f4e2b8025a080c3a
YHerm <0549405422.yh@gmail.com>
2024-12-19 13:17:45 +0200

    i think its working

--- apps/frontend/components/field/scoresheet/scoresheet-form.tsx
+++ apps/frontend/components/field/scoresheet/scoresheet-form.tsx
@@ -88,7 +88,7 @@ const ScoresheetForm: React.FC<ScoresheetFormProps> = ({
   const [resetDialog, setResetDialog] = useState<boolean>(false);
 
   const mode = useMemo(() => {
-    return scoresheet.status === 'waiting-for-gp' ? 'gp' : 'scoring';
+    return scoresheet.status === 'waiting-for-gp' || scoresheet.status === 'waiting-for-head-ref-gp' ? 'gp' : 'scoring';
   }, [scoresheet]);
 
   const getDefaultScoresheet = () => {
@@ -302,9 +302,9 @@ const ScoresheetForm: React.FC<ScoresheetFormProps> = ({
 
                 <Stack spacing={2} alignItems="center" my={6}>
                   {readOnly ||
-                  validatorErrors.length > 0 ||
-                  missionInfo.find(mi => mi.incomplete || !!mi.errors) ||
-                  (values.signature && values.signature.length > 0) ? (
+                    validatorErrors.length > 0 ||
+                    missionInfo.find(mi => mi.incomplete || !!mi.errors) ||
+                    (values.signature && values.signature.length > 0) ? (
                     <Image
                       src={values.signature || '/assets/scoresheet/blank-signature.svg'}
                       alt={`חתימת קבוצה #${team.number}`}
