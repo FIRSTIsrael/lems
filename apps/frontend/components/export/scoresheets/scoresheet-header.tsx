927a86d7e5e5237886011c521ebcdf34b4e16c5d
CoolGame8 <kuli.kaduri@gmail.com>
2024-12-24 19:02:39 +0200

    new scoresheet export

---
+++ apps/frontend/components/export/scoresheets/scoresheet-header.tsx
@@ -0,0 +1,41 @@
+import { WithId } from 'mongodb';
+import { Stack, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { DivisionWithEvent, Team, Scoresheet } from '@lems/types';
+import { SEASON_NAME } from '@lems/types';
+import { localizeDivisionTitle } from '../../../localization/event';
+
+interface ScoresheetHeaderProps {
+  division: WithId<DivisionWithEvent>;
+  team: WithId<Team>;
+  scoresheet: WithId<Scoresheet>;
+}
+
+export const ScoresheetHeader: React.FC<ScoresheetHeaderProps> = ({
+  division,
+  team,
+  scoresheet
+}) => {
+  return (
+    <Grid container>
+      <Grid size={10}>
+        <Stack justifyContent="space-between" height="100%">
+          <Typography fontSize="0.75rem" color="textSecondary">
+            הופק מתוך מערכת האירועים של <em>FIRST</em> ישראל ({scoresheet._id.toString()}) |{' '}
+            {localizeDivisionTitle(division)} | עונת <span dir="ltr">{SEASON_NAME}</span>
+          </Typography>
+          <Typography fontSize="1.75rem" fontWeight={700}>
+            דף ניקוד {scoresheet.round} של קבוצה #{team.number}
+          </Typography>
+        </Stack>
+      </Grid>
+      <Grid size={2}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          alt="לוגו של תוכניות FIRST LEGO League Challenge"
+          src="/assets/audience-display/sponsors/fllc-horizontal.svg"
+        />
+      </Grid>
+    </Grid>
+  );
+};
---
+++ apps/frontend/components/export/scoresheets/scoresheet-mission-clause.tsx
@@ -0,0 +1,91 @@
+import { WithId } from 'mongodb';
+import Markdown from 'react-markdown';
+import { Typography, ThemeProvider } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { Scoresheet } from '@lems/types';
+import { LocalizedMission, MissionClauseSchema } from '@lems/season';
+
+interface ScoresheetMissionClauseProps {
+  scoresheet: WithId<Scoresheet>;
+  missionIndex: number;
+  clauseIndex: number;
+  clause: MissionClauseSchema;
+  localizedMission: LocalizedMission;
+}
+
+export const ScoresheetMissionClause: React.FC<ScoresheetMissionClauseProps> = ({
+  scoresheet,
+  missionIndex,
+  clauseIndex,
+  clause,
+  localizedMission
+}) => {
+  let valueString: string | null = null;
+
+  try {
+    const value = scoresheet.data?.missions[missionIndex]?.clauses[clauseIndex]?.value;
+    if (clause.type === 'boolean') {
+      valueString = value ? 'כן' : 'לא';
+    } else if (clause.type === 'number') {
+      valueString = String(value ?? 0);
+    } else if (clause.type === 'enum' && clause.options) {
+      if (clause.multiSelect) {
+        valueString = (value as unknown as Array<string>)
+          .map(v => {
+            const optionIndex = (clause.options ?? []).indexOf(v);
+            const label = localizedMission.clauses[clauseIndex].labels?.[optionIndex];
+            return label;
+          })
+          .join(', ');
+      } else {
+        const optionIndex = clause.options.indexOf(value as string);
+        const label = localizedMission.clauses[clauseIndex].labels?.[optionIndex];
+        valueString = label ?? '';
+      }
+    }
+  } catch {
+    valueString = null;
+  }
+
+  return (
+    <ThemeProvider
+      theme={outerTheme => ({
+        ...outerTheme,
+        components: {
+          MuiToggleButton: {
+            styleOverrides: {
+              root: {
+                '&.Mui-selected': {
+                  '&:hover': {
+                    color: '#FFF',
+                    backgroundColor: '#81c784'
+                  },
+                  color: '#FFF',
+                  backgroundColor: '#2e7d32'
+                }
+              }
+            }
+          }
+        }
+      })}
+    >
+      <Grid alignItems="center" size={12} pb={0.35} pl={1}>
+        <Markdown skipHtml components={{ p: 'span' }}>
+          {localizedMission.clauses[clauseIndex].description}
+        </Markdown>
+        &nbsp;
+        <Typography
+          component="span"
+          sx={{
+            fontSize: '0.9rem',
+            color: '#d32f2f',
+            fontWeight: 'bold',
+            display: 'inline'
+          }}
+        >
+          {valueString}
+        </Typography>
+      </Grid>
+    </ThemeProvider>
+  );
+};
