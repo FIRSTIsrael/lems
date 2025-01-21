28473c63edf7c784051b177672f6e20aebf806d2
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2025-01-21 23:28:39 +0200

    add some more boilerplate

--- apps/backend/src/routers/public/portal/divisions/schedule.ts
+++ apps/backend/src/routers/public/portal/divisions/schedule.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
 import { ObjectId } from 'mongodb';
 import * as db from '@lems/database';
+import { JudgingSession, RobotGameMatch } from '@lems/types';
 
 const router = express.Router({ mergeParams: true });
 
@@ -8,12 +10,32 @@ router.get('/', (req: Request, res: Response) => {
   // get event outline
 });
 
-router.get('/judging', (req: Request, res: Response) => {
-  // get judging schedule
-});
+router.get(
+  '/judging',
+  asyncHandler(async (req: Request, res: Response) => {
+    const pipeline = [{ $match: { divisionId: new ObjectId(req.division._id) } }];
 
-router.get('/field', (req: Request, res: Response) => {
-  // get field schedule
-});
+    const schedule = await db.db
+      .collection<JudgingSession>('sessions')
+      .aggregate(pipeline)
+      .toArray();
+
+    res.json(schedule);
+  })
+);
+
+router.get(
+  '/field',
+  asyncHandler(async (req: Request, res: Response) => {
+    const pipeline = [{ $match: { divisionId: new ObjectId(req.division._id) } }];
+
+    const schedule = await db.db
+      .collection<RobotGameMatch>('matches')
+      .aggregate(pipeline)
+      .toArray();
+
+    res.json(schedule);
+  })
+);
 
 export default router;
