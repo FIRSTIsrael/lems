b49bfef222c275713b3b2f2d9cd5c3ea5db6a380
Nir Chen <bhrtkhvji@gmail.com>
2025-01-25 22:14:40 +0200

    implement components identical to @mui/lab using @mui/material

---
+++ apps/frontend/components/general/tab-managment.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+interface TabContextProps {
+  children?: React.ReactNode;
+  value: string;
+}
+
+const TabContext: React.FC<TabContextProps> = ({ value, children }) => {
+  return React.Children.map(children, child => {
+    if (React.isValidElement(child) && child.type === TabPanel) {
+      return React.cloneElement(child, { value } as React.Attributes);
+    }
+    return child;
+  });
+};
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  value: string;
+  index?: string;
+}
+
+const TabPanel: React.FC<TabPanelProps> = ({ children, value, index }) => {
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`}>
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+};
+
+export { TabContext, TabPanel };
