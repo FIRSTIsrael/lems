ef091042500c576fc78687b6d751d5186e540f05
LPNG1 <itamar.oryan@gmail.com>
2025-06-22 19:48:49 +0300

    Added middleware instead of route authorizer
    Co-authored-by: johnmeshulam <johnmeshulam@users.noreply.github.com>

--- apps/frontend/lib/emotion-cache.ts
+++
@@ -1,9 +0,0 @@
-import createCache from '@emotion/cache';
-import rtlPlugin from '@mui/stylis-plugin-rtl';
-import theme from './theme';
-
-export const createEmotionCache = () =>
-  createCache({
-    key: 'css',
-    stylisPlugins: theme.direction === 'rtl' ? [rtlPlugin] : []
-  });
