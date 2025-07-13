abf2e7a6f5915f045c148cf0aea5a16e6c63ff6a
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2025-07-13 20:58:26 +0300

    B O I L E R P L A T E

---
+++ libs/components/src/lib/components.tsx
@@ -0,0 +1,11 @@
+import styles from './components.module.css';
+
+export function Components() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Components!</h1>
+    </div>
+  );
+}
+
+export default Components;
---
+++ libs/localization/src/lib/localization.tsx
@@ -0,0 +1,11 @@
+import styles from './localization.module.css';
+
+export function Localization() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Localization!</h1>
+    </div>
+  );
+}
+
+export default Localization;
