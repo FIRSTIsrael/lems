9c680f75d8d06bf621056bdc9caa875edfd0a43b
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2025-07-14 19:19:15 +0300

    Fix props

--- libs/localization/src/lib/icons/chevron-end.tsx
+++ libs/localization/src/lib/icons/chevron-end.tsx
@@ -2,17 +2,15 @@ import { SvgIconProps, SxProps, useTheme } from '@mui/material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
-interface DirectionalIconProps extends SvgIconProps {
-  sxProps?: SxProps;
-}
+export const ChevronEndIcon: React.FC<SvgIconProps> = props => {
+  const { sx, ...rest } = props;
 
-export const ChevronEndIcon: React.FC<DirectionalIconProps> = ({ sxProps, ...props }) => {
   const theme = useTheme();
   const isRtl = theme.direction === 'rtl';
 
   return isRtl ? (
-    <ChevronLeftIcon {...props} sx={{ ...sxProps, color: 'rgba(0, 0, 0, 0.54)' }} />
+    <ChevronLeftIcon {...rest} sx={{ ...sx, color: 'rgba(0, 0, 0, 0.54)' }} />
   ) : (
-    <ChevronRightIcon {...props} sx={{ ...sxProps, color: 'rgba(0, 0, 0, 0.54)' }} />
+    <ChevronRightIcon {...rest} sx={{ ...sx, color: 'rgba(0, 0, 0, 0.54)' }} />
   );
 };
--- libs/localization/src/lib/icons/chevron-start.tsx
+++ libs/localization/src/lib/icons/chevron-start.tsx
@@ -1,18 +1,15 @@
-import { SvgIconProps, SxProps, useTheme } from '@mui/material';
+import { SvgIconProps, useTheme } from '@mui/material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
-interface DirectionalIconProps extends SvgIconProps {
-  sxProps?: SxProps;
-}
-
-export const ChevronStartIcon: React.FC<DirectionalIconProps> = ({ sxProps, ...props }) => {
+export const ChevronStartIcon: React.FC<SvgIconProps> = props => {
+  const { sx, ...rest } = props;
   const theme = useTheme();
   const isRtl = theme.direction === 'rtl';
 
   return isRtl ? (
-    <ChevronRightIcon {...props} sx={{ ...sxProps, color: 'rgba(0, 0, 0, 0.54)' }} />
+    <ChevronRightIcon {...rest} sx={{ ...sx, color: 'rgba(0, 0, 0, 0.54)' }} />
   ) : (
-    <ChevronLeftIcon {...props} sx={{ ...sxProps, color: 'rgba(0, 0, 0, 0.54)' }} />
+    <ChevronLeftIcon {...rest} sx={{ ...sx, color: 'rgba(0, 0, 0, 0.54)' }} />
   );
 };
