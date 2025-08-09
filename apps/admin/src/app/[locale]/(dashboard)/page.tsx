e545c43d5779043f0611144056e589121543e668
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2025-08-09 09:30:58 +0300

    Add title to homepage

--- apps/admin/src/app/[locale]/(dashboard)/page.tsx
+++ apps/admin/src/app/[locale]/(dashboard)/page.tsx
@@ -1,3 +1,12 @@
-export default function HomePage() {
-  return <p>Admin Dashboard</p>;
+import { getTranslations } from 'next-intl/server';
+import { Typography } from '@mui/material';
+
+export default async function HomePage() {
+  const t = await getTranslations('pages.index');
+
+  return (
+    <>
+      <Typography variant="h1">{t('title')}</Typography>
+    </>
+  );
 }
