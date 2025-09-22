0d93aab3f8efa3e6d2e5bbfc42b409c333a9ffd5
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2025-09-22 23:56:37 +0300

    fIX I18N STUFF

--- apps/portal/src/i18n/request.ts
+++ apps/portal/src/i18n/request.ts
@@ -3,12 +3,17 @@ import { hasLocale } from 'next-intl';
 import { routing } from './routing';
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  // Typically corresponds to the `[locale]` segment
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
 
+  const messages = (await import(`../../locale/${locale}.json`)).default;
+  const sharedMessages = (await import(`@lems/shared/locale/${locale}.json`)).default;
+
   return {
     locale,
-    messages: (await import(`../../locale/${locale}.json`)).default
+    messages: {
+      ...messages,
+      shared: { ...sharedMessages }
+    }
   };
 });
