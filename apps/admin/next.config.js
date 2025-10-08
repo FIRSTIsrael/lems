796f998cac5d1b03c8f06fb6f17bedd0942aa118
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2025-10-08 20:30:39 +0300

    didnt work

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -18,8 +18,7 @@ const nextConfig = {
 
   output: 'standalone',
 
-  // basePath: isProduction ? '/admin' : undefined,
-  basePath: '/admin',
+  basePath: isProduction ? '/admin' : undefined,
 
   reactStrictMode: true,
 
