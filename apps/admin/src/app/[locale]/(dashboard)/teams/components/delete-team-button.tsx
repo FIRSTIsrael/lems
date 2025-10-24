d3c94d590d6e61c06924d77c3925ae092a0cee0f
GevaX <gevax1010@gmail.com>
2025-10-24 15:34:03 +0300

    Add translation for tooltip

--- apps/admin/src/app/[locale]/(dashboard)/teams/components/delete-team-button.tsx
+++ apps/admin/src/app/[locale]/(dashboard)/teams/components/delete-team-button.tsx
@@ -3,6 +3,7 @@ import { GridActionsCellItem } from '@mui/x-data-grid';
 import { Team } from '@lems/types/api/admin';
 import { useState } from 'react';
 import { Tooltip } from '@mui/material';
+import { useTranslations } from 'next-intl';
 import { DeleteTeamDialog } from './delete-team-dialog';
 
 interface DeleteTeamButtonProps {
@@ -10,16 +11,13 @@ interface DeleteTeamButtonProps {
 }
 
 export const DeleteTeamButton: React.FC<DeleteTeamButtonProps> = ({ team }) => {
+  const t = useTranslations('pages.teams.list.delete');
   const [showDeletionDialog, setShowDeletionDialog] = useState(false);
   const isDisabled = team.status === 'active' || team.status === 'inactive';
 
   return (
     <>
-      <Tooltip
-        title={
-          isDisabled ? 'Cannot delete team which has participated in an event in the past' : ''
-        }
-      >
+      <Tooltip title={isDisabled ? t('disabled') : undefined}>
         <span>
           <GridActionsCellItem
             key="delete"
