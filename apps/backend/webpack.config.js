46137ef35ea3a5159942846100d05646dd196ccd
johnmeshulam <johnmeshulam@gmail.com>
2025-10-31 22:40:06 +0200

    Some package stuff

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -4,6 +4,5 @@ const { composePlugins, withNx } = require('@nx/webpack');
 module.exports = composePlugins(withNx(), config => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
-
   return config;
 });
