59717d9495e752d27f29b1c32b9b712485b65bb6
johnmeshulam <johnmeshulam@gmail.com>
2025-10-31 16:05:38 +0200

    Fix dev

--- apps/backend/src/lib/graphql/schema/index.ts
+++ apps/backend/src/lib/graphql/schema/index.ts
@@ -1,12 +1,12 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-// Read GraphQL schema files
-const baseTypeDefs = readFileSync(join(__dirname, 'base.graphql'), 'utf-8');
-const eventTypeDefs = readFileSync(join(__dirname, 'event.graphql'), 'utf-8');
-const divisionTypeDefs = readFileSync(join(__dirname, 'division.graphql'), 'utf-8');
-const teamTypeDefs = readFileSync(join(__dirname, 'team.graphql'), 'utf-8');
-const volunteerTypeDefs = readFileSync(join(__dirname, 'volunteer.graphql'), 'utf-8');
+const schemaDir = join(__dirname, './schema');
+const baseTypeDefs = readFileSync(join(schemaDir, 'base.graphql'), 'utf-8');
+const eventTypeDefs = readFileSync(join(schemaDir, 'event.graphql'), 'utf-8');
+const divisionTypeDefs = readFileSync(join(schemaDir, 'division.graphql'), 'utf-8');
+const teamTypeDefs = readFileSync(join(schemaDir, 'team.graphql'), 'utf-8');
+const volunteerTypeDefs = readFileSync(join(schemaDir, 'volunteer.graphql'), 'utf-8');
 
 export const typeDefs = [
   baseTypeDefs,
