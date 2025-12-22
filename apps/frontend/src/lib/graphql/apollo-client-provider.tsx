c80fa23b5186797a4afa7c813c005b22863d1955
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2025-12-22 18:00:25 +0200

    Revert "Ensure we always fetch fresh GraphQL data"
    This reverts commit b4967d971e9f1dc4259a69c9a7d4bddef2327e62.

--- apps/frontend/src/lib/graphql/apollo-client-provider.tsx
+++ apps/frontend/src/lib/graphql/apollo-client-provider.tsx
@@ -166,7 +166,7 @@ function makeClient(
         errorPolicy: 'all'
       },
       query: {
-        fetchPolicy: 'cache-and-network',
+        fetchPolicy: 'cache-first',
         errorPolicy: 'all'
       },
       mutate: {
