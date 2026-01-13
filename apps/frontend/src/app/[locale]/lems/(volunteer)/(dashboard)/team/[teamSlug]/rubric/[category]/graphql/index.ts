60afd15f22a3bfce84bc2ae1b8ca9b1d2d9c4ae1
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2026-01-13 23:18:42 +0200

    Fix build

--- apps/frontend/src/app/[locale]/lems/(volunteer)/(dashboard)/team/[teamSlug]/rubric/[category]/graphql/index.ts
+++ apps/frontend/src/app/[locale]/lems/(volunteer)/(dashboard)/team/[teamSlug]/rubric/[category]/graphql/index.ts
@@ -1,10 +1,4 @@
-export {
-  GET_RUBRIC_QUERY,
-  GET_AWARD_OPTIONS_QUERY,
-  GET_TEAM_SESSION_QUERY,
-  parseRubricData,
-  parseAwardOptions
-} from './query';
+export { GET_RUBRIC_QUERY, GET_TEAM_SESSION_QUERY, parseRubricData } from './query';
 export * from './types';
 
 export * from './mutations';
