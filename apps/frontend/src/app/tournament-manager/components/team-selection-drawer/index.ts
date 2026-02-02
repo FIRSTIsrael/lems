6a97cff94193992b81067285dd8e9ed7731d6b69
johnmeshulam <55348702+johnmeshulam@users.noreply.github.com>
2026-02-02 15:32:55 +0200

    remove bad file

--- apps/frontend/src/app/tournament-manager/components/team-selection-drawer/index.ts
+++
@@ -1,6 +0,0 @@
-export { SelectedSlotHeader } from './SelectedSlotHeader';
-export { FieldMatchesList } from './FieldMatchesList';
-export { JudgingSessionsList } from './JudgingSessionsList';
-export { SecondSlotInfo } from './SecondSlotInfo';
-export { ActionButtons } from './ActionButtons';
-export type { BaseSlotSectionProps, MatchesListProps, SessionsListProps } from './types';
