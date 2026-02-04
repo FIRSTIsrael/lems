8c920548bc32b6b031a750d0ef57acc79dfd6170
CoolGame8 <kuli.kaduri@gmail.com>
2026-02-04 12:01:35 +0200

    clean

--- apps/backend/src/lib/graphql/resolvers/mutations/schedule/index.ts
+++ apps/backend/src/lib/graphql/resolvers/mutations/schedule/index.ts
@@ -1,4 +1,3 @@
 export { swapMatchTeamsResolver } from './swap-match-teams';
 export { swapSessionTeamsResolver } from './swap-session-teams';
-export { setMatchParticipantTeamResolver } from './set-match-participant-team';
 export { setJudgingSessionTeamResolver } from './set-judging-session-team';
